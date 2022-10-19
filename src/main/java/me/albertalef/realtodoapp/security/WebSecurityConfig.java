dcc353f219efb1dc39a931ace1df6e31cf5fa53e
Albert Alef <albert_alef@hotmail.com>
2022-10-19 18:59:58 -0300

    fix(WebSecurityConfig): Fix the Cors bug

--- src/main/java/me/albertalef/realtodoapp/security/WebSecurityConfig.java
+++ src/main/java/me/albertalef/realtodoapp/security/WebSecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,15 +38,25 @@ public class WebSecurityConfig {
 	}
 
 
+
 	@Bean
 	public SecurityFilterChain filterChain( HttpSecurity http ) throws Exception{
 		http.csrf().disable();
+		http.cors();
 		http.authorizeRequests().anyRequest().authenticated();
 		http.httpBasic(Customizer.withDefaults());
 		http.formLogin();
 		http.logout();
-
 		return http.build();
-
+	}
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedMethods(List.of(("*")));
+		configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
